6937bf17a5878982af8c40592ce4edc2c7718555
Hossam <202227143@std.sci.cu.edu.eg>
2025-03-07 14:44:12 +0300

    first commit

---
+++ my-app/app/(coffee)/_layout.jsx
@@ -0,0 +1,5 @@
+import { Slot } from "expo-router";
+export default function CoffLayout(){
+
+    return <Slot/>
+}
---
+++ my-app/app/(tabs)/index.jsx
@@ -0,0 +1,70 @@
+import { View, Text,StyleSheet,ImageBackground,Pressable } from 'react-native'
+import React from 'react'
+import {Link} from 'expo-router'
+import ice from "@/assets/images/english-countryside-1.webp";
+
+
+const app = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={ice}
+      resizeMode='cover'
+      style={styles.image}
+      >
+      <Text style={styles.title}>thanks god</Text>
+      <Link href="/contact" style={{marginHorizontal:'auto'}} asChild>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}> contact us</Text>
+      </Pressable>
+     
+      </Link>
+      
+      </ImageBackground>
+     
+    </View>
+  )
+}
+ 
+export default app
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'red',
+    flex: 3,
+    flexDirection: 'column',
+  },
+    title: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: '900',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0,0,0,0.7)',
+    marginBottom: 120,
+
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '900',
+    textAlign: 'center',
+    padding:3,
+  
+
+  },
+  button:{
+
+    height: 40,borderRadius: 20,
+    backgroundColor: 'rgba(0,0,0,0.7)',
+    marginHorizontal: 20,
+    marginVertical: 10,
+    //justifyContent: 'center',
+    padding:6,
+  },
+  image: {
+   height: '100%',
+    width: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    resizeMode: 'cover', 
+  },
+  })   
